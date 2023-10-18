fa92218525840103c757ef4b26b4cf158fd9cdba
Vadym Dubniak <dubnyakv@gmail.com>
2023-10-18 11:15:28 +0300

    added readme

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,14 @@
 export const HomePage = () => (
-  <h1 className="title">
-    Home page
-  </h1>
+  <>
+    <h1 className="title">
+      Home page
+    </h1>
+
+    <p className="subtitle">
+      Welcome to list of post app!
+      <br/>
+      In order to add a post or comment, you need to authorize first.
+    </p>
+  </>
+  
 );
